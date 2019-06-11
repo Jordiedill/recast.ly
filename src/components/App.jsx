ac2026f4a61bba9ff1775b407b28f16114c4b0c8
jordiedill <jordan.dillard13@gmail.com>
2019-06-11 14:50:01 -0500

    finished video list with sample data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import VideoPlayer from "./VideoPlayer.js"
+import VideoList from "./VideoList.js"
+import exampleVideoData from "../data/exampleVideoData.js"
+import Search from "./Search.js"
+import VideoListEntry from "./VideoListEntry.js"
+
+
 var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        {/* <div><h5><em>search</em> view goes here</h5></div> */}
+        <Search />
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        {/* <div><h5><em>videoPlayer</em> view goes here</h5></div> */}
+        <VideoPlayer video={exampleVideoData[0]}/>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        {/* <div><h5><em>videoList</em> view goes here</h5></div> */}
+        <VideoList videos={exampleVideoData}/>
       </div>
     </div>
   </div>
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,38 +1,30 @@
-import exampleVideoData from "../data/exampleVideoData";
-
-class VideoListItems extends React.Component  {
-
-  constructor (props){
-    super(props);
-  }
-
-  render() {
-
-    return(
-      <div><h5><em>{this.props.item.title}</em> view goes here</h5></div>
-    )
+import VideoListEntry from "./VideoListEntry.js"
+import exampleVideoData from "../data/exampleVideoData.js"
 
 
-  }
+var VideoList = (props) => (
+  <div className="video-list">
+    
+    {console.log(props.videos)}
+    {props.videos.map(video =>
+        <VideoListEntry video={video} />
+    )}
+    
+    {/* <VideoListEntry movie={props.movies[0]}/>
+    <VideoListEntry movie={props.movies[1]}/>
+    <VideoListEntry movie={props.movies[2]}/>
+    <VideoListEntry movie={props.movies[3]}/>
+    <VideoListEntry movie={props.movies[4]}/> */}
+    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
 
+  </div>
+);
 
-}
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
-
-// VideoList.propTypes = {
-//   videos: React.PropTypes.array.isRequired
-// };
-
-const VideoList = (props) => {
-  <div className="video-list">
-    {props.items.map(item =>
-      <VideoListItems item={item}/>
-      )}
-  </div>
-}
-console.log(exampleVideoData);
-ReactDOM.render(<VideoList items={[exampleVideoData]}/>, document.getElementById("app"))
+VideoList.propTypes = {
+  videos: React.PropTypes.array.isRequired
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
